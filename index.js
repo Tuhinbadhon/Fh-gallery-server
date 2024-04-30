577e69ee026621f01199b48bc35c32556fcb082f
Md.Fazlul Haque <94878259+Tuhinbadhon@users.noreply.github.com>
2024-04-30 23:02:43 +0600

    env created

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You have successfully connected to MongoDB!"
     );
